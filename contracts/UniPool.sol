0e07a2fc480531b1900407ecb0108cf86032cb46
Raymond Pulver IV <raymondpulver@my.uri.edu>
2020-09-11 23:38:17 +0000

    fix Tok.sol misuse of msg.sender

--- contracts/UniPool.sol
+++ contracts/UniPool.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import { IUniswapV2Pair } from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {
+    IUniswapV2Pair
+} from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract UTap {
     IERC20 public Token;
