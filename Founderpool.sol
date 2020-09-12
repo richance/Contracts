38564411c695feb0e222660ef807415b387cd778
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2020-09-12 09:24:42 +0200

    Update and rename contracts/UniPool.sol.txt to Founderpool.sol

--- Founderpool.sol
+++ Founderpool.sol
@@ -1,31 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import {
-    IUniswapV2Pair
-} from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract UTap {
+contract Tap {
     IERC20 public Token;
-    IUniswapV2Pair public Pool;
     uint256 public blocklock;
     address public bucket;
 
     constructor(
         IERC20 Tokent,
-        address buckt,
-        IUniswapV2Pair Poolt
+        address buckt
     ) public {
         Token = Tokent;
         bucket = buckt;
-        Pool = Poolt;
     }
 
     function tap() public {
         require(blocklock <= now, "block");
-        Token.transfer(bucket, Token.balanceOf(address(this)) / 50);
-        blocklock = now + 1 days;
-        Pool.sync();
+        Token.transfer(bucket, Token.balanceOf(address(this)) / 40);
+        blocklock = now + 7 days;
     }
 }
