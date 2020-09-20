4c3454883a41eba5ce69fa5cfbd354fb06b1afa2
dillon <dillonkellar@protonmail.com>
2020-09-20 05:55:41 -0500

    renamed DonationBurn -> Donation

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
+
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 
 contract Donation {
     IERC20 public Token;
