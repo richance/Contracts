41dca993a6ee644a3b30468aa165fd760c0cb870
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2020-09-26 06:18:03 +0200

    Update Donation.sol

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -56,7 +56,7 @@ contract Donation {
       require(success, "Donation: Error transferring ether.");
     }
 
-function reset(uint256 am) public view returns (uint256){
+function getamountout(uint256 am) public view returns (uint256){
        uint256 public amout;
        amout = (am * 10 * (finish - start)) /
                 ((finish - start) - (now - start)));
