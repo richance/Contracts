d8f50a223623960cdd9d70509ad4f43f934c2116
richance <33940018+richance@users.noreply.github.com>
2020-11-02 17:33:03 +0100

    Update dist_pool_test.js

--- test/dist_pool_test.js
+++ test/dist_pool_test.js
@@ -13,7 +13,7 @@ describe("Distpool Constructor", function() {
 	
   // variable to store the deployed smart contract	
   let weth;	
-  let sprout;
+  let weth_rewards;
   let distpool;
   let owner, addr1, addr2, addr3, addr4;
 	
@@ -27,13 +27,13 @@ describe("Distpool Constructor", function() {
 	await weth.deployed();
 	
 	// tok
-	const Tok = await ethers.getContractFactory("Tok");
-    sprout = await Tok.deploy(deployer);
-	await sprout.deployed();
+	const Tok = await ethers.getContractFactory("WETH9");
+    weth_rewards = await Tok.deploy();
+	await weth_rewards.deployed();
 
 	// dist pool
 	const DistPool = await ethers.getContractFactory("StakingRewards");
-    distpool = await DistPool.deploy(deployer, deployer, sprout.address, weth.address);
+    distpool = await DistPool.deploy(deployer, deployer, weth_rewards.address, weth.address);
 	await distpool.deployed();
 	
   })		
@@ -65,7 +65,7 @@ describe("Distpool Notify", function() {
 	
   // variable to store the deployed smart contract	
   let weth;	
-  let sprout;
+  let weth_rewards;
   let distpool;
   let owner, addr1, addr2, addr3, addr4;
 	
@@ -79,13 +79,13 @@ describe("Distpool Notify", function() {
 	await weth.deployed();
 	
 	// tok
-	const Tok = await ethers.getContractFactory("Tok");
-    sprout = await Tok.deploy(deployer);
-	await sprout.deployed();
+	const Tok = await ethers.getContractFactory("WETH9");
+    weth_rewards = await Tok.deploy();
+	await weth_rewards.deployed();
 
 	// dist pool
 	const DistPool = await ethers.getContractFactory("StakingRewards");
-    distpool = await DistPool.deploy(owner.address, owner.address, sprout.address, weth.address);
+    distpool = await DistPool.deploy(owner.address, owner.address, weth_rewards.address, weth.address);
 	await distpool.deployed();
 	
   })		
@@ -97,23 +97,20 @@ describe("Distpool Notify", function() {
 
   it("Should call notify rewards", async function() {
 	  
-	//let balanceadsdr1 = await sprout.balanceOf(distpool.address);
-    //console.log(balanceadsdr1);	  
-	
-	//let allowance = await sprout.allowance(owner.address, distpool.address);
-    //console.log(allowance);	
+    const trans_obj = {
+        to: weth_rewards.address,
+        gasLimit: 4000000,
+		gasPrice: 4000000,
+        value: 10000000}
 	
     // can call this without balance!!!
-	await sprout.increaseAllowance(distpool.address, "10000000");
+	await owner.sendTransaction(trans_obj);
+	await weth_rewards.approve(distpool.address, "10000000");
+	await weth_rewards.transfer(distpool.address, "10000000");
 	
-	//allowance = await sprout.allowance(owner.address, distpool.address);
-    //console.log(allowance);		
-	
-	await sprout.transfer(distpool.address, "10000000");
     await distpool.notifyRewardAmount("10000000");
 	
-	balanceadsdr1 = await sprout.balanceOf(distpool.address);
-    //console.log(balanceadsdr1);		
+	balanceadsdr1 = await weth_rewards.balanceOf(distpool.address);
 	
   });
 
@@ -126,7 +123,7 @@ describe("Distpool Notify", function() {
         to: weth.address,
         gasLimit: 4000000,
 		gasPrice: 4000000,
-        value: 10000000}
+        value: 5000000}
 
     // get weth for addr1, approve and stake
 	
@@ -143,30 +140,104 @@ describe("Distpool Notify", function() {
     await addr2.sendTransaction(trans_obj);
     await weth.connect(addr2).approve(distpool.address, "10000000");	
     await distpool.connect(addr2).stake("5000000");
+	
+	// check the staking
+	expect(await distpool.totalSupply()).to.equal("10000000");
+	expect(await distpool.balanceOf(addr1.address)).to.equal("5000000");
+	expect(await distpool.balanceOf(addr2.address)).to.equal("5000000");
+	
+
+	expect(await weth.balanceOf(distpool.address)).to.equal("10000000");
+	expect(await weth.balanceOf(addr1.address)).to.equal("0");
+	expect(await weth.balanceOf(addr2.address)).to.equal("0");	
   });
 
   it("Should let useres stake 2", async function() {
 
-	let lastTimeRewardApplicable = await distpool.lastTimeRewardApplicable();
-    console.log(lastTimeRewardApplicable);
 
-	let rewardRate  = await distpool.rewardRate();
-    console.log(rewardRate );
-	
-	let rewards = await distpool.rewardPerToken();
-    console.log(rewards);
+    //let bn = provider.send("eth_getBlockByNumber")
+
 	
 	//increase time by one day
     await provider.send("evm_increaseTime", [60*60*24*1])  
 	await provider.send("evm_mine")  
 	
+	let lastTimeRewardApplicable = await distpool.lastTimeRewardApplicable();
+    console.log("lastTimeRewardApplicable: " + lastTimeRewardApplicable);
+
+	let rewardRate  = await distpool.rewardRate();
+    console.log("rewardRate " + rewardRate );
+
+	let earned  = await distpool.earned(addr1.address);
+    console.log("earned " + earned );
+	
+	let rewardPerToken = await distpool.rewardPerToken();
+    console.log("rewardPerToken: " + rewardPerToken);
+
+	let lastUpdateTime = await distpool.lastUpdateTime();
+    console.log("lastUpdateTime: " + lastUpdateTime);
+
+
+	let periodFinish = await distpool.periodFinish();
+    console.log("periodFinish: " + periodFinish);
+
+	let rewards = await distpool.rewards(addr1.address);
+    console.log("rewards addr1: " + rewards);
+
+	let userRewardPerTokenPaid = await distpool.userRewardPerTokenPaid(addr1.address);
+    console.log("userRewardPerTokenPaid addr1: " + userRewardPerTokenPaid);
+
+    await expect(distpool.connect(addr1).getReward()).to.emit(distpool, "RewardPaid"); //.withArgs(addr2.address,2);
+
+	 lastTimeRewardApplicable = await distpool.lastTimeRewardApplicable();
+    console.log("lastTimeRewardApplicable: " + lastTimeRewardApplicable);
+
+	 rewardRate  = await distpool.rewardRate();
+    console.log("rewardRate " + rewardRate );
+
+	earned  = await distpool.earned(addr1.address);
+    console.log("earned " + earned );
+	
+	 rewardPerToken = await distpool.rewardPerToken();
+    console.log("rewardPerToken: " + rewardPerToken);
+
+	 lastUpdateTime = await distpool.lastUpdateTime();
+    console.log("lastUpdateTime: " + lastUpdateTime);
+
+
+	 periodFinish = await distpool.periodFinish();
+    console.log("periodFinish: " + periodFinish);
+
+	 rewards = await distpool.rewards(addr1.address);
+    console.log("rewards addr1: " + rewards);
+
+	 userRewardPerTokenPaid = await distpool.userRewardPerTokenPaid(addr1.address);
+    console.log("userRewardPerTokenPaid addr1: " + userRewardPerTokenPaid);
+	
+  });
+
+  it("Should let useres stake 3", async function() {
+
+    const trans_obj = {
+        to: weth_rewards.address,
+        gasLimit: 4000000,
+		gasPrice: 4000000,
+        value: 10000000}
+	
+    // can call this without balance!!!
+	await owner.sendTransaction(trans_obj);
+	await weth_rewards.approve(distpool.address, "10000000");
+	await weth_rewards.transfer(distpool.address, "10000000");
+	
+    await distpool.notifyRewardAmount("10000000");
+	
+	
+	//increase time by one day
+    await provider.send("evm_increaseTime", [60*60*24*20]); //go after the end of the rewards period
+	await provider.send("evm_mine")  
+	
 	let poolamount = 10000000;
-	let days = 60*60*24*10;
-	
-	let people_stake = 5000000;
-	
-	let erg = poolamount / days * (people_stake);
-	console.log(erg);
+
 
 
     await expect(distpool.connect(addr1).getReward()).to.emit(distpool, "RewardPaid").withArgs(addr1.address,2);
