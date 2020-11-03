253a8facfa9e88607060c31477b0f38ee8e54c5e
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2020-11-03 11:05:40 +0200

    Update FounderPool.sol

--- contracts/FounderPool.sol
+++ contracts/FounderPool.sol
@@ -1,29 +1,31 @@
+// SPDX-License-Identifier: MIT// SPDX-License-Identifier: MIT
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract FounderPool {
-    IERC20 public Token;
-    uint256 public blocklock;
-    address public bucket;
+  IERC20 public Token;
+  uint256 public blocklock;
+  address public bucket;
 
-    constructor(
-        IERC20 Tokent,
-        address buckt
-    ) public {
-        Token = Tokent;
-        bucket = buckt;
+  constructor(IERC20 Tokent, address buckt) public {
+    Token = Tokent;
+    bucket = buckt;
+  }
+
+  uint256 public balmin;
+
+  function tap() public {
+    require(blocklock <= now, "block");
+    if (balmin < Token.balanceOf(address(this)) / 24) {
+      balmin = Token.balanceOf(address(this)) / 24;
     }
-    uint256 public bal;
-    function tap() public {
-        require(blocklock <= now, "block");
-          if (bal <= Token.balanceOf(address(this))) {
-          bal = Token.balanceOf(address(this));
-          }
-        Token.transfer(bucket, Token.balanceOf(address(this)) / 24);
-        blocklock = now + 14 days;
+    if (balmin > Token.balanceOf(address(this))) {
+      Token.transfer(bucket, Token.balanceOf(address(this)));
+    } else {
+      Token.transfer(bucket, balmin);
     }
+    blocklock = now + 14 days;
+  }
 }
